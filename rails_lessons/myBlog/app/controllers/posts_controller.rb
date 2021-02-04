89e37462cf460acc23218bc03dbe039bf6c57fde
Mizuki Onogawa <onogawa.mizuki@gmail.com>
2021-02-05 02:54:27 +0900

    20210205

---
+++ rails_lessons/myBlog/app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+    # Convention over Configuration (CoC)
+  
+    def index
+      @posts = Post.all.order(created_at: 'desc')
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+      puts "----------test-----------"
+      p @post
+      p @post.id
+      p @post.title
+      p @post.body
+      puts "----------test-----------"
+    end
+  
+  end
